f93d336365d5e55a9dbc4d4d2f7ccb67a85d197a
weijun-sh <huangweijun0322@163.com>
2021-02-02 15:20:02 +0800

    add config

---
+++ web3/mpc/config.js
@@ -0,0 +1,6 @@
+//------ config -----------------------------------------------------
+global.account_keystore = '../../testcases/test-account-passwd-123456--0xa33d3ecc57Bd3F9C06Baf2CAbF5f3ecc7df8d6c8' //TODO
+global.account_password = '../../testcases/passwd' //TODO
+global.mpcPublicKey = '045ca26401515c6d6e08e055734e71d866110dbd13efa9487ebed43d4a97adfe1fe189b5b71d24af889993e754d3767cd2b2c9473ab4448bbe8b0309ad1838af09' //TODO
+global.address_to = '0xe2bcaF8F9F56a6D4880C168FA8fa02b02bFD19e0' //TODO
+global.custom_data = 'recordtest' //TODO
---
+++ web3/user/config.js
@@ -0,0 +1,6 @@
+//------ config -----------------------------------------------------
+global.account_keystore = '../../testcases/test-account-passwd-123456--0xa33d3ecc57Bd3F9C06Baf2CAbF5f3ecc7df8d6c8' //TODO
+global.account_password = '../../testcases/passwd' //TODO
+global.address_to = '0xe2bcaF8F9F56a6D4880C168FA8fa02b02bFD19e0' //TODO
+global.custom_data = 'recordtest' //TODO
+
